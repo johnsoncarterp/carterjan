911df9b93e1a5817267f6dad390a154690c58741
carter johnson <carter32lax@yahoo.com>
2015-03-05 13:59:43 -0800

    on video  11 time 4:44

--- js/entities/entities.js
+++ js/entities/entities.js
@@ -11,7 +11,12 @@ game.PlayerEntity = me.Entity.extend({
                 }
             }]);
 
-        this.body.setVelocity(5, 0);
+        this.body.setVelocity(5, 20);
+
+        this.renderable.addAnimation("idle", [78]);
+        this.renderable.addAnimation("walk", [117, 118, 119, 120, 121, 122, 123, 124, 125], 80);
+
+        this.renderable.setCurrentAnimation("idle");
 
     },
     update: function(delta) {
@@ -19,11 +24,25 @@ game.PlayerEntity = me.Entity.extend({
             //adds to the position of my x to set velocity
             //me.timer.tick makes everything look smooth
             this.body.vel.x += this.body.accel.x * me.timer.tick;
+            this.flipX(true);
         } else {
             this.body.vel.x = 0;
         }
 
+        if (this.body.vel.x !== 0) {
+            if (!this.renderable.isCurrentAnimation("walk")) {
+                this.renderable.setCurrentAnimation("walk");
+            }
+        }else{
+            this.renderable.setCurrentAnimation("idle");
+        }
         this.body.update(delta);
+
+        this._super(me.Entity, "update", [delta]);
         return true;
     }
+});
+
+game.PlayerBaseEntity = me.Entity.extend({
+    
 });
