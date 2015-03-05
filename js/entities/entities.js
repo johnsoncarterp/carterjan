a58249c7350b53c4e35ca7c6e2761b243024dab5
carter johnson <carter32lax@yahoo.com>
2015-03-05 08:17:47 -0800

    guys moves/ on video 8

--- js/entities/entities.js
+++ js/entities/entities.js
@@ -1,6 +1,6 @@
 game.PlayerEntity = me.Entity.extend({
     init: function(x, y, settings) {
-         this._super(me.Entity, 'init', [x, y, {
+        this._super(me.Entity, 'init', [x, y, {
                 image: "player",
                 spritewidth: "64",
                 spriteheight: "64",
@@ -9,15 +9,21 @@ game.PlayerEntity = me.Entity.extend({
                 getShape: function() {
                     return (new me.Rect(0, 0, 64, 64)).toPolygon();
                 }
-         }]);
-//     
-//     this.body.setVelocity(5, 0);
-     
+            }]);
+
+        this.body.setVelocity(5, 0);
+
     },
-    
-    update: function() {
-        if(me.input.isKeyPressed("right")){
-           this.body.vel.x += this.body.accel.x * me.timer.tick;
+    update: function(delta) {
+        if (me.input.isKeyPressed("right")) {
+            //adds to the position of my x to set velocity
+            //me.timer.tick makes everything look smooth
+            this.body.vel.x += this.body.accel.x * me.timer.tick;
+        } else {
+            this.body.vel.x = 0;
         }
+
+        this.body.update(delta);
+        return true;
     }
 });
