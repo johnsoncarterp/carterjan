30dccce9b6aea258304bc6a0b6f048194b8d9928
carter johnson <carter32lax@yahoo.com>
2015-03-25 14:04:24 -0700

    on video 28

--- js/screens/title.js
+++ js/screens/title.js
@@ -4,6 +4,26 @@ game.TitleScreen = me.ScreenObject.extend({
 	 */
 	onResetEvent: function() {	
 		me.game.world.addChild(new me.Sprite(0, 0, me.loader.getImage('title-screen')), -10); // TODO
+                
+                me.input.bindKey(me.input.KEY.INPUT, "start");
+                
+                me.game.world.addChild(new (me.Renderable.extend({
+                    init: function(){
+                        this._super(me.Renderable, 'init', [510, 30, me.game.viewport.width, me.game.viewport.height]);
+                        this.font = new me.Font("Ariel", 46, "white");
+                    },
+                    
+                    draw: function(renderer){
+                        this.font.draw(renderer.getContext(), "nauts", 450, 130);
+                        this.font.draw(renderer.getContext(), "Press ENTER To Play", 250, 530);
+                    }
+                })));
+                
+                this.handler = me.event.subscribe(me.event.KEYDOWN, function(action, keyCode, edge){
+                  if(action === "start"){
+                      me.state.change(me.state.PLAY);
+                  }  
+                });
 	},
 	
 	
@@ -11,6 +31,7 @@ game.TitleScreen = me.ScreenObject.extend({
 	 *  action to perform when leaving this screen (state change)
 	 */
 	onDestroyEvent: function() {
-		; // TODO
+		me.input.unbindKey(me.input.key.ENTER); // TODO
+                me.event.unsubscribe(this.handler);
 	}
 });
