3d0314cbf2a45f3d6bcdaf613c3cfa630a1c5249
carter johnson <carter32lax@yahoo.com>
2015-04-17 13:49:51 -0700

    on video 53

--- js/game.js
+++ js/game.js
@@ -2,9 +2,9 @@
 /* Game namespace */
 var game = {
 // an object where to store game information
-data: {
+    data: {
 // score
-score: 0,
+        score: 0,
         enemyBaseHealth: 10,
         playerBaseHealth: 10,
         enemyCreepHealth: 4,
@@ -37,52 +37,56 @@ score: 0,
         win: "",
         pausePos: "",
         buytext: ""
-},
-        // Run on page load.
-        "onload": function() {
+    },
+    // Run on page load.
+    "onload": function() {
         // Initialize the video.
         if (!me.video.init("screen", me.video.CANVAS, 1067, 600, true, '1.0')) {
-        alert("Your browser does not support HTML5 canvas.");
-                return;
+            alert("Your browser does not support HTML5 canvas.");
+            return;
         }
 
         // add "#debug" to the URL to enable the debug Panel
         if (document.location.hash === "#debug") {
-        window.onReady(function() {
-        me.plugin.register.defer(this, debugPanel, "debug");
-        });
+            window.onReady(function() {
+                me.plugin.register.defer(this, debugPanel, "debug");
+            });
         }
         me.save.add({exp: 0, exp1: 0, exp2: 0, exp3: 0, exp4: 0});
-        
+
         me.state.SPENDEXP = 112;
-        
+        me.state.LOAD = 113;
+        me.state.NEW = 114;
+
         console.log(game.data.exp);
         console.log(game.data.exp2);
 
         // Initialize the audio.
         me.audio.init("mp3,ogg");
-                // Set a callback to run when loading is complete.
-                me.loader.onload = this.loaded.bind(this);
-                // Load the resources.
-                me.loader.preload(game.resources);
-                // Initialize melonJS and display a loading screen.
-                me.state.change(me.state.LOADING);
-        },
-        // Run on game resources loaded.
-        "loaded": function() {
-                me.pool.register("player", game.PlayerEntity, true);
-                me.pool.register("PlayerBase", game.PlayerBaseEntity);
-                me.pool.register("EnemyBase", game.EnemyBaseEntity);
-                me.pool.register("EnemyCreep", game.EnemyCreep, true);
-                me.pool.register("GameTimerManager", game.GameTimerManager);
-                me.pool.register("HeroDeathManager", game.HeroDeathManager);
-                me.pool.register("ExperienceManager", game.ExperienceManager);
-                me.pool.register("SpendGold", game.SpendGold);
-                
-                me.state.set(me.state.MENU, new game.TitleScreen());
-                me.state.set(me.state.PLAY, new game.PlayScreen());
-                me.state.set(me.state.SPENDEXP, new game.SpendExp());
-                // Start the game.
-                me.state.change(me.state.MENU);
-        }
+        // Set a callback to run when loading is complete.
+        me.loader.onload = this.loaded.bind(this);
+        // Load the resources.
+        me.loader.preload(game.resources);
+        // Initialize melonJS and display a loading screen.
+        me.state.change(me.state.LOADING);
+    },
+    // Run on game resources loaded.
+    "loaded": function() {
+        me.pool.register("player", game.PlayerEntity, true);
+        me.pool.register("PlayerBase", game.PlayerBaseEntity);
+        me.pool.register("EnemyBase", game.EnemyBaseEntity);
+        me.pool.register("EnemyCreep", game.EnemyCreep, true);
+        me.pool.register("GameTimerManager", game.GameTimerManager);
+        me.pool.register("HeroDeathManager", game.HeroDeathManager);
+        me.pool.register("ExperienceManager", game.ExperienceManager);
+        me.pool.register("SpendGold", game.SpendGold);
+
+        me.state.set(me.state.MENU, new game.TitleScreen());
+        me.state.set(me.state.PLAY, new game.PlayScreen());
+        me.state.set(me.state.SPENDEXP, new game.SpendExp());
+        me.state.set(me.state.LOAD, new game.LoadProfile());
+        me.state.set(me.state.NEW, new game.NewProfile());
+        // Start the game.
+        me.state.change(me.state.MENU);
+    }
 };
